d0b27abde79e8369f8703431371f0264bf105240
gui-hmm <henriquemelomoura@gmail.com>
2025-09-23 05:04:12 -0300

    first commit

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 
 if (!getApps().length) {
   initializeApp(environment.firebase);
