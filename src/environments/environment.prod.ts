0f45248ef342eb78b27c4fffd520f8255d7f2176
gui-hmm <henriquemelomoura@gmail.com>
2025-09-22 22:45:44 -0300

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'SUA_API_KEY_AQUI',
-    authDomain: 'SEU_AUTH_DOMAIN_AQUI',
-    projectId: 'SEU_PROJECT_ID_AQUI',
-    storageBucket: 'SEU_STORAGE_BUCKET_AQUI',
-    messagingSenderId: 'SEU_MESSAGING_SENDER_ID_AQUI',
-    appId: 'SEU_APP_ID_AQUI',
-    measurementId: 'SEU_MEASUREMENT_ID_AQUI'
+    apiKey: 'FIREBASE_API_KEY_AQUI',
+    authDomain: 'FIREBASE_AUTH_DOMAIN_AQUI',
+    projectId: 'FIREBASE_PROJECT_ID_AQUI',
+    storageBucket: 'FIREBASE_STORAGE_BUCKET_AQUI',
+    messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID_AQUI',
+    appId: 'FIREBASE_APP_ID_AQUI',
+    measurementId: 'FIREBASE_MEASUREMENT_ID_AQUI'
   }
 };
